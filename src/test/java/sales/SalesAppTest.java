4ebb5288a6049a5238564be3d223ee196e0349b3
zzwgo <kenzzw@163.com>
2019-08-10 22:22:36 +0800

    Add:test for getSales method

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -5,9 +5,7 @@ import org.junit.Test;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class SalesAppTest {
@@ -36,7 +34,24 @@ public class SalesAppTest {
 
         assertNotNull(sales);
     }
+    @Test
+    public void should_return_sales_when_call_getSales_given_wrong_time(){
+        SalesApp salesApp = spy(new SalesApp());
+        Date today = new Date();
+        Date yesterday = new Date(today.getTime()-86400000L);
+        Date tomorrow = new Date(today.getTime()-86400000L);
 
+        Sales mockSales= mock(Sales.class);
+        when(mockSales.getEffectiveFrom()).thenReturn(yesterday);
+        when(mockSales.getEffectiveTo()).thenReturn(tomorrow);
+        SalesDao salesDao=mock(SalesDao.class);
+        when(salesDao.getSalesBySalesId(any())).thenReturn(mockSales);
+        doReturn(salesDao).when(salesApp).getSalesDao();
+
+        Sales sales=salesApp.getSales(any());
+
+        assertNull(sales);
+    }
     @Test
     public void should_return_List_contains_Time_when_call_getHeaders_given_true(){
         SalesApp salesApp = new SalesApp();
@@ -45,6 +60,7 @@ public class SalesAppTest {
 
         assertTrue(list.contains("Time"));
     }
+
     @Test
     public void should_return_List_contains_Time_when_call_getHeaders_given_false(){
         SalesApp salesApp = new SalesApp();
@@ -53,4 +69,6 @@ public class SalesAppTest {
 
         assertTrue(list.contains("Local Time"));
     }
+
+
 }
